aa0bff3bb37a622328345a1972bec4124de32fe1
IYUI <314754801@qq.com>
2021-04-19 23:10:02 +0800

    init

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -1,20 +1,44 @@
-﻿// RayTracing.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<iostream>
+#include"vec3.h"
+#include"color.h"
+#include"ray.h"
+using std::cin;
+using std::cout;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+//实现渐变色
+color ray_color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	//线性插值
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+int main() {
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	// Image
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// Camera
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j) {
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;//进度指示器
+		for (int i = 0; i < image_width; ++i) {
+			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::cerr << "\nDone.\n";
+}
