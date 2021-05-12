9e8763bbd680c2b1dfb9d6d200e7cf43f41f45c7
IYUI <314754801@qq.com>
2021-05-12 22:25:50 +0800

    update

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -1,5 +1,5 @@
 ﻿#include "rtweekend.h"
-
+#include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
@@ -7,6 +7,10 @@
 #include"vec3.h"
 #include"color.h"
 #include"ray.h"
+#include "material.h"
+
+#include<cstdlib>
+#include<ctime>
 using std::cin;
 using std::cout;
 
@@ -27,10 +31,17 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
 
 
 //根据球体法向量着色
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
-	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color(1, 1, 1));
+
+	if (depth <= 0)
+		return color(0, 0, 0);
+	if (world.hit(r, 0.001, infinity, rec)) {
+		ray scattered;
+		color attenuation;
+		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+			return attenuation * ray_color(scattered, world, depth - 1);
+		return color(0, 0, 0);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
@@ -40,37 +51,45 @@ color ray_color(const ray& r, const hittable& world) {
 int main() {
 
 	// Image
-	const auto aspect_ratio = 16.0 / 9.0;
+	const auto aspect_ratio = 16.0 / 16.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
 	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// Camera
-
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	//视口左下角的坐标
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	camera cam;
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+	clock_t start, end;
+	start = clock();
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;//进度指示器
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
+	end = clock();
+	double endtime = (double)(end - start) / CLOCKS_PER_SEC;
+	std::cerr << "\nRendered in "<<endtime<<"s \n";
 	std::cerr << "\nDone.\n";
 }
